926cbaaa91ee1140d727e6919f12c71f5b3bce18
Sam Super <samsuper@Sams-Mac-mini.local>
2023-07-10 19:27:10 -0700

    Django Flash messages

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Post
+from .forms import PostForm
 
-posts = [
-    {
-        'title': 'Beautiful is better than ugly',
-        'author': 'John Doe',
-        'content': 'Beautiful is better than ugly',
-        'published_at': 'October 1, 2022'
-    },
-    {
-        'title': 'Explicit is better than implicit',
-        'author': 'Jane Doe',
-        'content': 'Explicit is better than implicit',
-        'published_at': 'October 1, 2022'
-    }
-]
+
+def create_post(request):
+    if request.method == 'GET':
+        context = {'form': PostForm()}
+        return render(request,'blog/post_form.html',context)
+    elif request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The post has been created successfully.')
+            return redirect('posts')
+        else:
+            messages.error(request, 'Please correct the following errors:')
+            return render(request,'blog/post_form.html',{'form':form})
+
 
 
 def home(request):
-    context = {
-        'posts': posts
-    }
+    posts = Post.objects.all()
+    context = {'posts': posts}
     return render(request, 'blog/home.html', context)
 
 
