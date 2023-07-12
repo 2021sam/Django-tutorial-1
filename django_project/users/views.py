a59ae5e280c90bd4200962285a3b1b1e6d9fcfa9
Sam Super <samsuper@Sams-Mac-mini.local>
2023-07-11 17:15:57 -0700

    Django Registration

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+
+def sign_up(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'users/register.html', {'form': form})    
+   
+    if request.method == 'POST':
+        form = RegisterForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return redirect('posts')
+        else:
+            messages.error(request, 'Form is not yet valid.')
+            return render(request, 'users/register.html', {'form': form})
+
+
 
 
 def sign_in(request):
