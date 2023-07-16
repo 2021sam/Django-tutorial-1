2923eede34184b3ab85d7595c64542c0f9fee178
Sam Super <samsuper@Sams-Mac-mini.local>
2023-07-16 14:22:07 -0700

    Spelling correction

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -17,7 +17,7 @@ def sign_up(request):
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            messages.success(request, 'You have singed up successfully.')
+            messages.success(request, 'You have successfully signed up.')
             login(request, user)
 
 
