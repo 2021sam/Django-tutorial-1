4be26f328aed6e13ea60efdd15fc3931a5f79d0f
Sam Super <samsuper@Sams-Mac-mini.local>
2023-07-16 15:29:48 -0700

    Add mobile

--- django_project/streetcred/models.py
+++ django_project/streetcred/models.py
@@ -23,6 +23,8 @@ class Profile(models.Model):
     work_city_address = models.TextField(max_length=50, blank=True)
     woirk_state_address = models.TextField(max_length=50, blank=True)
     work_zip_address = models.TextField(max_length=50, blank=True)
+    mobile_cell_number = models.TextField(max_length=14, blank=True)
+    # (456) 456-1234       Will need to format.
 
 
 #this method to generate profile when user is created
