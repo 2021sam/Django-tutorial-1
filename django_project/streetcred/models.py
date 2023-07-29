c3051a9759ac249b429b1c9e67d43707759a00f9
2021sam <7050592@gmail.com>
2023-07-29 13:25:27 -0700

    Re arrange default

--- django_project/streetcred/models.py
+++ django_project/streetcred/models.py
@@ -38,7 +38,7 @@ class Profile(models.Model):
     mobile_cell_number = models.TextField(max_length=14, blank=True)
     willing_to_relocate = models.BooleanField(default=False)
     # (456) 456-1234       Will need to format.
-    work_authorization = models.CharField(max_length=34, default='select work authorization', choices=CHOICES_WORK_AUTHORIZATION)
+    work_authorization = models.CharField(max_length=34, choices=CHOICES_WORK_AUTHORIZATION, default='select work authorization')
     # https://stackoverflow.com/questions/51623747/django-best-way-to-create-a-multiple-choice-field
     def __str__(self):
         return f'{self.user}, {self.address}'
